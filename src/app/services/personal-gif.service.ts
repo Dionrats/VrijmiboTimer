85cb0e63cde34d30139fd8e2d0487fa3c4273567
Niels Korporaal <nap.korporaal@gmail.com>
2025-02-09 17:21:09 +0100

    refactor: hardcoded gifCount vervangen door JSON object

--- src/app/services/personal-gif.service.ts
+++ src/app/services/personal-gif.service.ts
@@ -1,19 +1,34 @@
 import { Injectable } from '@angular/core';
 import { IGifRetriever } from './igif-retriever';
 import { Observable, of } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class PersonalGifService implements IGifRetriever {
-  private basePath = '/assets/personalgifs/'
-  private gifCount = 21;
+  private basePath = '/assets/personalgifs/';
+  private gifsPath = '/assets/personalgifs.json';
+  private gifs: string[];
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.fetchGifFilenames();
+  }
+
+  private fetchGifFilenames() {
+    this.http
+      .get<{ gifs: string[] }>(this.gifsPath)
+      .subscribe((data) => {
+        this.gifs = data.gifs;
+      });
+  }
 
   retrieveGif(context?: string): Observable<string> {
-    let number = Math.floor(Math.random() * this.gifCount);
-    number = number == 0 ? 1 : number;
-    return of(this.basePath + number + ".gif");
+    if (!this.gifs) {
+      return of('');
+    }
+    const randomIndex = Math.floor(Math.random() * this.gifs.length);
+    const gifUrl = this.basePath + this.gifs[randomIndex];
+    return of(gifUrl);
   }
 }
